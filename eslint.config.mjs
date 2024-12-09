de9ac118f2d5f2fd5bbb1f3c86180abd33d65c1a
Sandros94 <sandro.circi@digitoolmedia.com>
2024-12-09 20:24:00 +0100

    up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,7 @@ export default createConfigForNuxt({
         }],
 
         // Project specific overrides
+        '@typescript-eslint/no-empty-object-type': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         'vue/multi-word-component-names': 'off',
       },
