5a7902bd1304ba9b5bee47a247130170c75d577b
Sandros94 <sandro.circi@digitoolmedia.com>
2024-12-11 00:09:28 +0100

    fix: throw error when `usePGlite` composable is being called server side

--- src/runtime/app/composables/pglite.ts
+++ src/runtime/app/composables/pglite.ts
@@ -1,7 +1,13 @@
 import {
+  createError,
   useNuxtApp,
 } from '#imports'
 
 export function usePGlite() {
+  if (import.meta.server) throw createError({
+    statusCode: 500,
+    statusMessage: 'Client-side only',
+    message: '[pglite] `usePGlite()` composable should only be called client-side',
+  })
   return useNuxtApp().$pglite
 }
