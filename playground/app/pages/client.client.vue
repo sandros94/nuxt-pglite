d9dbbc7961c16db2487876e98bb6c079579f448e
Sandros94 <sandro.circi@digitoolmedia.com>
2024-12-12 13:14:36 +0100

    up

--- playground/app/pages/client.client.vue
+++ playground/app/pages/client.client.vue
@@ -10,14 +10,11 @@
     <h2>
       PGlite is {{ db?.ready ? 'ready' : 'not ready' }}
     </h2>
-    <button @click="reset()">
-      Reset DB
+    <button @click="clear()">
+      Delete Data
     </button>
     <button @click="insert()">
       Insert
-    </button>
-    <button @click="query()">
-      Query
     </button>
     <pre v-if="data">
       {{ data }}
@@ -29,54 +26,15 @@
 </template>
 
 <script setup lang="ts">
-import type { Results } from '@electric-sql/pglite'
-import type { LiveQuery } from '@electric-sql/pglite/live'
-
 interface Record {
   id: number
   name: typeof names[number]
 }
 
 const names = ['Buonarroti', 'Da Vinci', 'di Niccolò di Betto Bardi', 'Sanzio'] as const
-const data = ref<Results<Record> | undefined>()
-let live: LiveQuery<Record> | undefined = undefined
+const data = useLiveQuery<Record>('SELECT * FROM test;')
 
 const db = usePGlite()
-
-async function reset() {
-  if (live) {
-    await live.unsubscribe()
-  }
-  await db.exec(`
-DROP TABLE IF EXISTS test;
-CREATE TABLE IF NOT EXISTS test (
-  id SERIAL PRIMARY KEY,
-  name TEXT
-);
-`).then(async () => {
-    await query()
-    subscribe()
-  })
-}
-
-await query()
-  .catch(async () => await reset())
-  .then(async () => await subscribe())
-
-async function query() {
-  return data.value = await db.query<Record>('SELECT * FROM test;')
-}
-
-async function subscribe() {
-  if (live) return
-  live = await db.live.query<Record>('SELECT * FROM test;', [], (res) => {
-    data.value = res
-  })
-}
-onBeforeRouteLeave(() => {
-  if (!live) return
-  live.unsubscribe()
-})
 
 async function insert() {
   await db
@@ -87,4 +45,16 @@ async function insert() {
       ],
     )
 }
+
+async function clear() {
+  await db
+    .query('DELETE FROM test;')
+}
+
+await callOnce('pglite:db:init', async () => {
+  await db.exec(`CREATE TABLE IF NOT EXISTS test (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);`)
+})
 </script>
